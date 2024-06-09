26eb005da94f5b3e1ebc5b5057e69cfa8957e733
Franco Rojas <fvrl.cr@gmail.com>
2024-06-08 19:58:43 -0600

    Cambios en la ui principal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
+  const [title, setTitle] = useState('')
+
+  const openModal = (op, id, name) => {
+    setTitle('')
+    switch (op) {
+      case 1:
+        setTitle('Consulta de Facturas')
+        break
+      case 2:
+        setTitle('Consulta de Cuenta')
+        break
+      default:
+        break
+    }
+  }
 
   return (
     <>
@@ -18,18 +33,66 @@ function App() {
         <h4>Factura y Estado de Cuenta</h4>
       </div>
       <div className="group-content">
-        <card className="card" onClick={() => window.location.href = '/factura'}>
+        <card className="card" onClick={() => openModal(1)}
+          data-bs-toggle='modal' data-bs-target='#modalConsultaFacturas'>
           <div className="card-body">
             <img src={facturaLogo} alt="Factura" />
             <h5 className="card-title">Facturación</h5>
           </div>
         </card>
-        <card className="card" onClick={() => window.location.href = '/cuenta'}>
+        <card className="card" onClick={() => openModal(2)}
+          data-bs-toggle='modal' data-bs-target='#modalConsultaCuenta'>
           <div className="card-body">
             <img src={cuentaLogo} alt="Cuenta" />
             <h5 className="card-title">Estado de Cuenta</h5>
           </div>
         </card>
+      </div>
+
+      <div id='modalConsultaFacturas' className='modal fade' aria-hidden='true'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title'>{title}</h5>
+              <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+            </div>
+            <div className='modal-body'>
+              <form>
+                <div className='mb-3'>
+                  <label htmlFor='inputNumero' className='form-label'>Número de Teléfono</label>
+                  <input type='text' className='form-control' id='inputNumero' />
+                </div>
+              </form>
+            </div>
+            <div className='modal-footer'>
+              <button type='button' className='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
+              <button type='button' className='btn btn-primary'>Consultar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id='modalConsultaCuenta' className='modal fade' aria-hidden='true'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title'>{title}</h5>
+              <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+            </div>
+            <div className='modal-body'>
+              <form>
+                <div className='mb-3'>
+                  <label htmlFor='inputNumero' className='form-label'>Número de Teléfono</label>
+                  <input type='text' className='form-control' id='inputNumero' />
+                </div>
+              </form>
+            </div>
+            <div className='modal-footer'>
+              <button type='button' className='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
+              <button type='button' className='btn btn-primary'>Consultar</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
